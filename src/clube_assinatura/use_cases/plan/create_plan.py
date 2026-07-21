b82e9676d8750184527268402783c38672933ebb
Yasmin Manuela <manuelayasminsc@gmail.com>
2026-07-21 09:27:44 -0300

    Adiciona camada de use cases

---
+++ src/clube_assinatura/use_cases/plan/create_plan.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+
+from clube_assinatura.domain.models.plan import Plan
+from clube_assinatura.domain.repositories.plan_repository import PlanRepository
+
+
+@dataclass
+class CreatePlanData:
+    name: str
+    description: str
+    price: int
+    currency: str
+    interval: str
+    stripe_product_id: str
+    stripe_price_id: str
+
+
+class CreatePlan:
+    """Cria um novo plano de assinatura."""
+
+    def __init__(self, repository: PlanRepository):
+        self._repository = repository
+
+    async def execute(self, data: CreatePlanData):
+        plan = Plan(
+            name=data.name,
+            description=data.description,
+            price=data.price,
+            currency=data.currency,
+            interval=dataput.interval,
+            stripe_product_id=data.stripe_product_id,
+            stripe_price_id=data.stripe_price_id,
+        )
+        return await self._repository.save(plan)
